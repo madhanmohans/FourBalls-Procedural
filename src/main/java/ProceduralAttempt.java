01ffed4acd28a84fc78a01fd0624f8d1387dc938
madhanmohans <sherlockholmes.missme@gmail.com>
2024-05-09 12:26:56 +0530

    drew Four balls with Procedural attempt

--- src/main/java/ProceduralAttempt.java
+++ src/main/java/ProceduralAttempt.java
@@ -1,18 +1,20 @@
 import processing.core.PApplet;
 
 public class ProceduralAttempt extends PApplet {
-    private final float speed = 1;
-    private float firstBall = speed;
-    private float secondBall = speed;
-    private float thirdBall = speed;
-    private float forthBall = speed;
-
-    private final int height = 720, width = 720;
+    private final float ballSpeed = 1;
+    private final int ballHeight = 10;
+    private final int ballWidth = 10;
+    private float firstBallXCoordinate, secondBallXCoordinate, thirdBallXCoordinate, forthBallXCoordinate  = 1;
+    private final int screenHeight = 720, screenWidth = 720;
+    private final float firstBallYCoordinate = (float) screenHeight / 5;
+    private final float secondBallYCoordinate = 2 * firstBallYCoordinate;
+    private final float thirdBallYCoordinate = 3 * firstBallYCoordinate;
+    private final float forthBallYCoordinate = 4 * firstBallYCoordinate;
 
     @Override
     public void settings() {
         super.settings();
-        size(width, height);
+        size(screenWidth, screenHeight);
     }
 
     @Override
@@ -22,8 +24,14 @@ public class ProceduralAttempt extends PApplet {
 
     @Override
     public void draw() {
-        ellipse(firstBall, (float)height / 5, 10, 10);
-        firstBall += speed;
+        ellipse(firstBallXCoordinate, firstBallYCoordinate, ballHeight, ballWidth);
+        firstBallXCoordinate += ballSpeed;
+        ellipse(secondBallXCoordinate, secondBallYCoordinate, ballHeight, ballWidth);
+        secondBallXCoordinate += 2 * ballSpeed;
+        ellipse(thirdBallXCoordinate, thirdBallYCoordinate, ballHeight, ballWidth);
+        thirdBallXCoordinate += 3 * ballSpeed;
+        ellipse(forthBallXCoordinate, forthBallYCoordinate, ballHeight, ballWidth);
+        forthBallXCoordinate += 4 * ballSpeed;
     }
 
     public static void main(String[] args) {
