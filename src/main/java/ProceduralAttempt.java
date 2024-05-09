48a322371e2b23e51a11c834566d698325df2fad
madhanmohans <sherlockholmes.missme@gmail.com>
2024-05-09 12:06:26 +0530

    initialized variables and main

---
+++ src/main/java/ProceduralAttempt.java
@@ -0,0 +1,13 @@
+public class ProceduralAttempt {
+    private final float speed = 1;
+    private float firstBall = speed;
+    private float secondBall = speed;
+    private float thirdBall = speed;
+    private float forthBall = speed;
+
+    private final float height = 12, width = 10;
+
+    public static void main(String[] args) {
+
+    }
+}
